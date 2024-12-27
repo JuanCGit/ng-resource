772f6644b87e1d11dba85e95b3a56b20aa7c76bf
Juan Casanova <juancasanovagago@gmail.com>
2024-12-27 15:53:57 +0100

    feat: add signals logic and prime ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, signal } from '@angular/core';
+import { Select } from 'primeng/select';
+import { IColor, INumber, ITexture } from '../interfaces/select.interfaces';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Select],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng-resource';
+  numbers: INumber[] = [
+    { number: 'One', value: 1 },
+    { number: 'Two', value: 2 },
+    { number: 'Three', value: 3 },
+    { number: 'Four', value: 4 },
+  ];
+  colors: IColor[] = [
+    { color: 'Red', value: 'red' },
+    { color: 'Green', value: 'green' },
+    { color: 'Blue', value: 'blue' },
+    { color: 'Yellow', value: 'yellow' },
+  ];
+  textures: ITexture[] = [
+    { texture: 'Hairy', value: 'hairy' },
+    { texture: 'Viscous', value: 'viscous' },
+    { texture: 'Hard', value: 'hard' },
+    { texture: 'Liquid', value: 'liquid' },
+  ];
+  selectedNumber = signal<number | undefined>(undefined);
+  selectedColor = signal<string | undefined>(undefined);
+  selectedTexture = signal<string | undefined>(undefined);
+  aiQuery = computed(() => {
+    const number = this.selectedNumber();
+    const color = this.selectedColor();
+    const texture = this.selectedTexture();
+    const allSelected = ![number, color, texture].includes(undefined);
+    return allSelected
+      ? `Make an image of number ${number} with ${texture} texture and ${color} color`
+      : undefined;
+  });
+
+  onNumberChange(selected: INumber): void {
+    this.selectedNumber.set(selected.value);
+  }
+
+  onColorChange(selected: IColor): void {
+    this.selectedColor.set(selected.value);
+  }
+
+  onTextureChange(selected: ITexture): void {
+    this.selectedTexture.set(selected.value);
+  }
 }
---
+++ src/interfaces/select.interfaces.ts
@@ -0,0 +1,14 @@
+export interface INumber {
+  number: string;
+  value: number;
+}
+
+export interface IColor {
+  color: string;
+  value: string;
+}
+
+export interface ITexture {
+  texture: string;
+  value: string;
+}
