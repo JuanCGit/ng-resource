57010262e984891cceb1e0c62169ce551f9ebfd4
Juan Casanova <juancasanovagago@gmail.com>
2024-12-31 08:41:39 +0100

    display environment and config deployment

---
+++ environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  openAI: {
+    apiKey: 'INSERT YOUR API KEY HERE',
+    apiUrl: 'https://api.openai.com/v1/images/generations',
+    model: 'dall-e-3',
+  },
+};
