190bbb1526e830c4c5c01ffafb02bba99b8552ef
Juan Casanova <juancasanovagago@gmail.com>
2025-01-03 10:07:04 +0100

    fix: revert last commit

--- environment.ts
+++ environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: false,
   openAI: {
-    apiKey:
-      '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',
+    apiKey: 'INSERT YOUR API KEY HERE',
     apiUrl: 'https://api.openai.com/v1/images/generations',
     model: 'dall-e-3',
   },
