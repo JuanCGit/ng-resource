149a88f1c20e01bd03e3043f276ecd2b7e6ca837
Juan Casanova <juancasanovagago@gmail.com>
2025-02-07 21:04:24 +0100

    feat: provide zoneless detection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -9,7 +13,6 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
     providePrimeNG({
@@ -18,5 +21,6 @@ export const appConfig: ApplicationConfig = {
       },
     }),
     provideHttpClient(),
+    provideExperimentalZonelessChangeDetection(),
   ],
 };
